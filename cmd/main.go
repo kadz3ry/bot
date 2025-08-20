97c8f0fe66e9f9c1ea07f1add55f05019162153b
kadz3ry <nomangeorgy@gmail.com>
2025-08-20 14:08:19 +0300

    First iteration

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	bot, err := tgbotapi.NewBotAPI("7788693016:AAEUMGW3JqSmS4GWt2FacrYyEUwosEO-tn0")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+			//msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
 }
