aadaed1298862ca25d4059fccca528b164fe010a
kadz3ry <nomangeorgy@gmail.com>
2025-08-20 16:38:49 +0300

    test

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("7788693016:AAEUMGW3JqSmS4GWt2FacrYyEUwosEO-tn0")
+	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
